2507f0eb407d9c41f4bea7212766f967c82a61d0
Søren Søndergaard <s.sondergaard@icloud.com>
2021-03-01 19:58:01 +0100

    Add logic

--- src/HelloWorld/Program.cs
+++ src/HelloWorld/Program.cs
@@ -4,9 +4,37 @@ namespace HelloWorld
 {
     class Program
     {
+        private readonly ITextWriter _writer;
+
+        public Program(ITextWriter writer)
+        {
+            _writer = writer;
+        }
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var prg = new Program(new ConsoleTextWriter());
+            prg.SayHello();
+        }
+
+        private void SayHello()
+        {
+            _writer.WriteLine("Hello world");
         }
     }
+
+
+    internal interface ITextWriter
+    {
+        void WriteLine(string lineToWrite);
+    }
+
+    internal class ConsoleTextWriter : ITextWriter
+    {
+        public void WriteLine(string lineToWrite)
+        {
+            Console.WriteLine(lineToWrite);
+        }
+    }
+    
 }
